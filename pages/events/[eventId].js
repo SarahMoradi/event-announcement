ab97ca2e38b3f50d6f04b563a28341326a9a0bb0
Sarah Moradi <moradisarah2001@gamil.com>
2022-12-29 16:54:11 +0330

    added event details while click on each event

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,32 @@
+import EventContent from '../../components/event-detail/event-content'
+import EventLogistics from '../../components/event-detail/event-logistics'
+import EventSummary from '../../components/event-detail/event-summary'
+import { Fragment } from 'react'
+import { getEventById } from '../../dummy-data'
+import { useRouter } from 'next/router'
+
 const EventDetailsPage = () => {
+  const route = useRouter()
+  const eventId = route.query.eventId
+  const event = getEventById(eventId)
+
+  if (!event) {
+    return <p>No event found!</p>
+  }
+
   return (
-    <div>
-      <h1>Event Detail Page</h1>
-    </div>
+    <Fragment>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   )
 }
 
