912b6686666c8ae6d8bce5827fbd03878d938a3b
Sarah Moradi <moradisarah2001@gmail.com>
2023-01-08 15:56:09 +0330

    add wrapper for _app to optimize all pages details

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import '../styles/globals.css'
 
+import Head from 'next/head'
 import Layout from '../components/layout/layout'
 
 function MyApp({ Component, pageProps }) {
   return (
     <Layout>
+      <Head>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='description' content='This is the event platform' />
+        <title>Next event platform</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   )
