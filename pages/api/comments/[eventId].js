610f9ed1e2d7632690a7a67a47c5baee3783754d
Sarah Moradi <moradisarah2001@gmail.com>
2023-01-14 00:26:42 +0330

    complete eventId api route

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,12 +1,36 @@
-function handler(req, res){
-    const eventId = req.query.enetId;
-     
-    if(req.method === "POST"){
-        const  {email, name, text} = req.body;
+function handler(req, res) {
+  const eventId = req.query.eventId
+
+  if (req.method === 'POST') {
+    const { email, name, text } = req.body
+    if (
+      !email ||
+      !name ||
+      !text ||
+      !email.includes('@') ||
+      name.trim() === ' ' ||
+      text.trim() === ''
+    ) {
+      res.status(422).json({ message: 'Invalid input.' })
+      return
     }
-    if(req.method === "GET"){}
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    }
+    console.log(newComment)
+
+    res.status(201).json({ message: 'Added comment.', commnet: newComment })
+  }
+  if (req.method === 'GET') {
+    const dummyList = [
+      { id: 'c1', name: 'sarah', text: 'First Comment' },
+      { id: 'c2', name: 'moradi', text: 'Second Comment' },
+    ]
+    res.status(200).json({ comments: dummyList })
+  }
 }
-
-
 
 export default handler
