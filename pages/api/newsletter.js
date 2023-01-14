9ab059a7c8e228176ad1f6d1531682e0c5a58059
Sarah Moradi <moradisarah2001@gmail.com>
2023-01-14 23:05:48 +0330

    add mongoDB configs

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+const { MongoClient, ServerApiVersion } = require('mongodb')
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email
 
@@ -6,8 +8,15 @@ function handler(req, res) {
       res.status(422).json({ message: 'Invalid Email Address.' })
       return
     }
+
+    const client = await MongoClient.connect(
+      'mongodb+srv://captainHook:O9KAJAJYz8PGzNo5@cluster0.l9uc8g1.mongodb.net/?retryWrites=true&w=majority'
+    )
+    const db = client.db('newsletter')
+    await db.collection('emails').insertOne({ email: userEmail })
+    client.close()
+
     res.status(201).json({ message: 'Your Email Was Suucessfully Verified.' })
-    console.log(userEmail, 'this is user email.')
   }
 }
 export default handler
