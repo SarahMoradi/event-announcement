44891473f8a56701c35911a7f46567edd6a2229c
Sarah Moradi <moradisarah2001@gmail.com>
2023-01-15 20:30:35 +0330

    complete error handling for newsletter

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,5 +1,16 @@
 import { MongoClient } from 'mongodb'
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    'mongodb+srv://sarah:4floYFa0MKtvWMcf@cluster0.l9uc8g1.mongodb.net/?retryWrites=true&w=majority'
+  )
+  return client
+}
+async function insertDocument(client, document) {
+  const db = client.db('newsletter')
+  await db.collection('emails').insertOne(document)
+}
+
 async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email
@@ -9,13 +20,22 @@ async function handler(req, res) {
       return
     }
 
-    const client = await MongoClient.connect(
-      'mongodb+srv://sarah:4floYFa0MKtvWMcf@cluster0.l9uc8g1.mongodb.net/?retryWrites=true&w=majority'
-    )
-    const db = client.db('newsletter')
-    await db.collection('emails').insertOne({ email: userEmail })
-    client.close()
+    let client ;
+    try {
+      client = await connectDatabase()
+    } catch (error) {
+      res.status(500).json({message: 'Failed to connect.'})
+      return
+    }
 
+    try {
+      await insertDocument(client, { email: userEmail })
+      client.close()
+    } catch (error) {
+      res.status(500).json({message: 'Failed to insert to database.'})
+      return
+    }
+  
     res.status(201).json({ message: 'Your Email Was Suucessfully Verified.' })
   }
 }
