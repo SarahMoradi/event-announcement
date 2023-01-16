551e8f42cf7b1cb4736b7eb052d556275f7e6486
Sarah Moradi <moradisarah2001@gmail.com>
2023-01-16 15:39:47 +0330

    add notification context details

--- pages/_app.js
+++ pages/_app.js
@@ -3,18 +3,24 @@ import '../styles/globals.css'
 import Head from 'next/head'
 import Layout from '../components/layout/layout'
 import Notification from '../components/ui/notification'
+import { NotificationContextProvider } from '../store/notification-context'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta name='description' content='This is the event platform' />
-        <title>Next event platform</title>
-      </Head>
-      <Component {...pageProps} />
-      <Notification title="Test" message="This is test" status="pending" />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <meta
+            name='viewport'
+            content='initial-scale=1.0, width=device-width'
+          />
+          <meta name='description' content='This is the event platform' />
+          <title>Next event platform</title>
+        </Head>
+        <Component {...pageProps} />
+        <Notification title='Test' message='This is test' status='pending' />
+      </Layout>
+    </NotificationContextProvider>
   )
 }
 export default MyApp
--- store/notification-context.js
+++ store/notification-context.js
@@ -1,9 +1,32 @@
-import { createContext } from 'react'
+import { createContext, useState } from 'react'
 
 const NotificationContext = createContext({
-  Notification: null,
-  showNotification: function () {},
+  notification: null,
+  showNotification: function (notificationData) {},
   hideNotification: function () {},
 })
+
+export function NotificationContextProvider(props) {
+  const [activeNotification, setActiveNotification] = useState()
+
+  function showNotificationHandler(notificationData) {
+    setActiveNotification(notificationData)
+  }
+
+  function hideNotificationHandler() {
+    setActiveNotification(null)
+  }
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  }
+
+  return (
+    <NotificationContext.Provider value={context}>
+      {props.children}
+    </NotificationContext.Provider>
+  )
+}
 
 export default NotificationContext
